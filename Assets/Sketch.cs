9f0b7627a55210895ca11d5d4844627708779873
monicabahan <monicapbahan@gmail.com>
2016-09-30 14:27:29 +1300

    added files

---
+++ Assets/Sketch.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sketch : MonoBehaviour {
+
+	public GameObject myPrefab;
+
+	void Start () {
+		int totalCubes = 30;
+
+		float totalDistance = 2.9f;
+
+		// LINEAR DISTRO
+	/*	for (int i = 0; i < totalCubes; i++) 
+		{
+			float perc = i / (float)totalCubes;
+
+			float x = perc * totalDistance;
+			float y = 3.0f;
+			float z = 0.0f;
+			var newCube = (GameObject)Instantiate (myPrefab, new Vector3 (x, y, z), Quaternion.identity);
+			newCube.GetComponent<CubeScript> ().SetSize (1.0f - perc);
+			newCube.GetComponent<CubeScript> ().rotateSpeed = 0; 
+		} */ 
+
+		// SIN() DISTRO
+		for (int i = 0; i < totalCubes; i++) 
+		{
+			float perc = i / (float)totalCubes;
+			float sin = Mathf.Sin (perc * Mathf.PI/2);
+
+			float x = 1.8f + sin * totalDistance;
+			float y = 5.0f;
+			float z = 0.0f;
+
+			var newCube = (GameObject)Instantiate (myPrefab, new Vector3 (x, y, z), Quaternion.identity);
+
+			newCube.GetComponent<CubeScript> ().SetSize (.45f * (1.0f - perc) );
+			newCube.GetComponent<CubeScript> ().rotateSpeed = .2f + perc*4.0f; 
+		}
+	}
+
+	void Update () {
+	
+	}
+}
